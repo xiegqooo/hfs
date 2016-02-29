c54b53e96586e8c7ad72224f3fa3e8369711d06f
xiegqooo <xiegqooo@hotmail.com>
2016-02-29 14:17:31 +0800

    添加 发布日志

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hfs
 上传代码 
+发布日志
+* [Release](https://github.com/xiegqooo/hfs/releases)
--- s.cmd
+++ s.cmd
@@ -1,2 +1,6 @@
+echo off
 set NODE_ENV=development
-node app
+rem node-debug app.js
+node --debug app
+rem node app
+rem node --debug-brk=41268 --nolazy app.js 
